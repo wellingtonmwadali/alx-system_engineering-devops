42f36d6b245300c600facca6555afb889d62b9ee
Mwadali <wellingtonmwadali@gmail.com>
2023-10-11 09:01:48 -0700

    cor

--- 0x16-api_advanced/100-main.py
+++ 0x16-api_advanced/100-main.py
@@ -11,12 +11,3 @@ if __name__ == '__main__':
         print("Ex: {} programming 'python java javascript'".format(sys.argv[0]))
     else:
         result = count_words(sys.argv[1], [x for x in sys.argv[2].split()])
-bob@dylan $             
-bob@dylan $ python3 100-main.py programming 'react python java javascript scala no_results_for_this_one'
-java: 27
-javascript: 20
-python: 17
-react: 17
-scala: 4
-bob@dylan $ python3 100-main.py programming 'JavA java'
-java: 54
